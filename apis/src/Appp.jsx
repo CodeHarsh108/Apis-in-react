50dc72f1cd8ad4f9e7aba6c1c78e0a8171ebd834
Harsh Patil <itsharshhh6@gmail.com>
2025-06-30 08:57:35 +0530

    Post using axiom

--- apis/src/Appp.jsx
+++ apis/src/Appp.jsx
@@ -1,6 +1,24 @@
+import axios from 'axios';
 import './App.css';
+import React, { useState } from 'react';
 
 function Appp() {
+    const [data, setData] = useState();
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newpost = {
+            'title': 'New Post',
+            'body': 'This is the body of the new post',
+            'userId': 1
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts', newpost)
+        .then(response => {
+            console.log('Post added successfully:', response.data);
+            setData([response.data]);
+        })
+    };
     return(
         <div>
             <form onSubmit={handleSubmit}>
